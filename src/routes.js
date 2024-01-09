aae1ac412864e393caacb8d57ef8656c789136a0
Wellton Quirino <welltonq@outlook.com>
2024-01-09 19:59:52 -0300

    CRUD realized

---
+++ src/routes.js
@@ -0,0 +1,81 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { search } = req.query
+
+      const tasks = database.select('tasks', {
+        title: search,
+        description: search
+      })
+
+      return res.end(JSON.stringify(tasks))
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description, completed_at } = req.body
+
+      if (!title) {
+        return res.writeHead(400).end(
+          JSON.stringify({ message: 'title is required' }),
+        )
+      }
+
+      if (!description) {
+        return res.writeHead(400).end(
+          JSON.stringify({message: 'description is required' })
+        )
+      }
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at,
+        created_at: new Date(),
+        updated_at: new Date(),
+      }
+
+      database.insert('tasks', task)
+
+      return res.writeHead(201).end('Task created success!')
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { title, description, completed_at } = req.body
+
+      database.update('tasks', id, {
+        title,
+        description,
+        completed_at,
+      })
+
+      return res.writeHead(204).end('Task updated success!')
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      database.delete('tasks', id)
+
+      return res.writeHead(204).end('Task deleted com sucesso!')
+    }
+  },
+]
