334e75f2a96a67eee2feee98a5d2409f703910d6
Wellton Quirino <welltonq@outlook.com>
2024-01-14 19:04:29 -0300

    finished challenge 01 - Node.js

--- src/routes.js
+++ src/routes.js
@@ -56,15 +56,49 @@ export const routes = [
     path: buildRoutePath('/tasks/:id'),
     handler: (req, res) => {
       const { id } = req.params
-      const { title, description, completed_at } = req.body
+      const { title, description } = req.body
+
+      if (!title && !description) {
+        return res.writeHead(400).end(
+          JSON.stringify({ message: 'title or description are required' })
+        )
+      }
+
+      const [task] = database.select('tasks', { id })
+
+      if (!task) {
+        return res.writeHead(404).end()
+      }
 
       database.update('tasks', id, {
-        title,
-        description,
-        completed_at,
+        title: title ?? task.title,
+        description: description ?? task.description,
+        updated_at: new Date()
       })
 
       return res.writeHead(204).end('Task updated success!')
+    }
+  },
+  {
+    method: 'PATCH',
+    path: buildRoutePath('/tasks/:id/complete'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      const [task] = database.select('tasks', { id })
+
+      if (!task) {
+        return res.writeHead(404).end()
+      }
+
+      const isTaskCompleted = !!task.completed_at
+      const completed_at = isTaskCompleted ? null : new Date()
+
+      database.update('tasks', id, {
+        completed_at,
+      })
+
+      return res.writeHead(204).end('Task success completed!')
     }
   },
   {
